cde799b7b2232059a32f3b443055dbf5ad8983e7
Kishan Trivedi <trivedikishan12345@gmail.com>
2023-12-15 14:54:41 -0800

    Added know more button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import yourImage from '../placeholder.png'; // Adjust the path accordingly
 import SocialMedia from './SocialMedia';
 import styles from "./Hero.module.css"
-import { right } from '@popperjs/core';
+import { FaArrowDown } from 'react-icons/fa';
 
 const Hero = () => {
     return(
         <>
         <div className="container-fluid">
             <div className={`row justify-content-center ${styles.verticalCenter}`} style={{height:"100vh"}}>
-                <div className="col-sm-10 col-md-9 text-light text-center">
+                <div className="col-sm-10 col-md-9 text-light text-center position-relative">
                 <div className={styles.hover_animation}>
                         <h1 data-text="I'm Kishan Trivedi" className={styles.h1_element}></h1>
                 </div>
@@ -18,6 +18,12 @@ const Hero = () => {
                 </div>
                 <div style={{height:"75px"}}></div>
                 <SocialMedia />
+                <button className={`btn btn-primary  ${styles.scrollMore}`}>
+                    Know More!
+                    <div class={`position-absolute bottom-0 end-0  ${styles.bounceArrow}`}>
+                        <FaArrowDown size={20} color="#fff" />
+                    </div>
+                </button>
                 </div>
                 <div className="col-sm-3 d-none d-lg-block" style={{textAlign:"end"}}>
                     <img className={`${styles.hero_image}`} src={yourImage} alt="" />
