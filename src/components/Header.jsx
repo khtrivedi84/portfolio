5b8081eceee97438ffb4f569219d8f4bd982c61f
Kishan Trivedi <trivedikishan12345@gmail.com>
2024-04-15 19:21:56 -0500

    Added new nav items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
 import styles from './Header.module.css';
 import profile from "../images/logo.png";
 import { useEffect, useState } from 'react';
-
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoMdContact } from "react-icons/io";
+import { FaCode } from "react-icons/fa";
+import { FaCodeBranch } from "react-icons/fa";
+import { TiUser } from "react-icons/ti";
+import { FaSuitcase } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa";
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
 
@@ -31,12 +38,14 @@ const Header = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className={`navbar-nav nav-pills justify-content-center w-100 ${styles.headerBackground} ${styles.new_ul}`}>
-                        <a href="#aboutme" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">About</li></a>
-                        <a href="#skills" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">Skills</li></a>
-                        <a href="#projects" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">Projects</li></a>
-                        <a href="#contact" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">Contact Me</li></a>
-                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/khtrivedi/" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">LinkedIn</li></a>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/khtrivedi84" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item">GitHub</li></a>
+                        <a href="#aboutme" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><TiUser /> About</li></a>
+                        <a href="#skills" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaCode /> Skills</li></a>
+                        <a href="#projects" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaCodeBranch /> Projects</li></a>
+                        <a href="#experience" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaSuitcase /> Experience</li></a>
+                        <a href="#education" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaGraduationCap /> Education</li></a>
+                        <a href="#contact" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><IoMdContact /> Contact Me</li></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/khtrivedi/" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaLinkedin /> LinkedIn</li></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/khtrivedi84" className={`nav-link ${styles.customNavLink}`} ><li className="nav-item"><FaGithub /> GitHub</li></a>
                         <li className="nav-item"><Link to="" className={`nav-link ${styles.resumeBtn}`}>Resume <IoCloudDownloadOutline /></Link></li>
                     </ul>
                 </div>
