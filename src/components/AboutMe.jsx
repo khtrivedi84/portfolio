38734ce5f44514ef26dbb45ce52c9b9e5453593c
Kishan Trivedi <trivedikishan12345@gmail.com>
2024-04-18 23:10:55 -0500

    Added animations

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import styles from "./AboutMe.module.css";
 import htmlLogo from "../images/html_logo.png";
 import jsLogo from "../images/js_logo.png";
@@ -13,6 +14,11 @@ import dockerLogo from "../images/docker_logo.png";
 import profile from "../images/final2.png";
 
 const AboutMe = () => {
+
+    const { ref, inView } = useInView({
+        threshold: 0.5, // Trigger when 50% of the element is in view
+        triggerOnce: true // Only trigger once
+        });
   useEffect(() => {
     const skillsItems = document.querySelectorAll(`.${styles.skills__item}`);
     skillsItems.forEach(item => {
@@ -22,14 +28,38 @@ const AboutMe = () => {
   }, []);
 
   return (
-    <div id="aboutme" className={`${styles.centerDiv} ${styles.aboutMeMobile} container`}>
+    <div ref={ref} id="aboutme" className={`${styles.centerDiv} ${styles.aboutMeMobile} container`}>
         <div className={`row justify-content-center align-items-center text-white`}>
-            <h2 className="text-center text-light " style={{marginBottom:"2rem"}}>About Me</h2>
-            <div className="col-md-6 justify-content-center d-flex mb-4 ">
-                <div className={`${styles.imageDiv} ${styles.glowingBorder}`}>
+            <h2 className="text-center text-light" style={{marginBottom:"2rem"}}>About Me</h2>
+            <div className={`col-md-6 justify-content-center d-flex mb-4`}>
+                <div className={`${styles.imageDiv} ${styles.hideDiv} ${styles.glowingBorder} ${inView ? styles.animateFromLeft : ''}`}>
                     <img src={profile} alt="HTML" className={`${styles.profileImage}`} />
                 </div>
+            </div>
+            <div className="col-sm-12 col-md-6">
+                <div className={`about-me ${styles.hideDiv} ${inView ? styles.animateFromRight : ''}`}>
+                    <p className="about-me__text">
+                        Hello! 👋 I'm a seasoned computer engineer with a passion for code and innovation, holding a Master's in Computer Science and Engineering from Santa Clara University. Proficient in Python, JavaScript, Data Structures, Algorithms, and various frameworks, I navigate the dynamic interplay between creativity and technology in web development.
+                    </p>
+                    <p className="about-me__text">
+                        Beyond the tech realm, catch me exploring the latest trends, enjoying a hike 🏞️, or uncovering hidden city gems. Whether tackling code challenges or blazing new trails, I'm always ready for an adventure.
+                    </p>
+                    <p className={`about-me__text d-md-block ${styles.optionalPara}`}>
+                        I have experience working with a variety of technologies, including Django, Flask, SQL, React, Redux, PostgreSQL, and more. I am always looking to learn new technologies and expand my skillset.
+                    </p>
+                    <p className="about-me__text">
+                        Let's connect and share stories—exploring the exciting journey where technology meets imagination. Ready to embark on this journey together?
+                    </p>
+                </div>
+            </div>
             
+        </div>
+    </div>
+  );
+};
+
+export default AboutMe;
+
                 {/* <div className={styles.skills}>
                     
                     <div className={styles.skills__row}>
@@ -93,27 +123,3 @@ const AboutMe = () => {
                         </div>
                     </div>
                 </div> */}
-            </div>
-            <div className="col-sm-12 col-md-6">
-                <div className="about-me">
-                    <p className="about-me__text">
-                        Hello! 👋 I'm a seasoned computer engineer with a passion for code and innovation, holding a Master's in Computer Science and Engineering from Santa Clara University. Proficient in Python, JavaScript, Data Structures, Algorithms, and various frameworks, I navigate the dynamic interplay between creativity and technology in web development.
-                    </p>
-                    <p className="about-me__text">
-                        Beyond the tech realm, catch me exploring the latest trends, enjoying a hike 🏞️, or uncovering hidden city gems. Whether tackling code challenges or blazing new trails, I'm always ready for an adventure.
-                    </p>
-                    <p className={`about-me__text d-md-block ${styles.optionalPara}`}>
-                        I have experience working with a variety of technologies, including Django, Flask, SQL, React, Redux, PostgreSQL, and more. I am always looking to learn new technologies and expand my skillset.
-                    </p>
-                    <p className="about-me__text">
-                        Let's connect and share stories—exploring the exciting journey where technology meets imagination. Ready to embark on this journey together?
-                    </p>
-                </div>
-            </div>
-            
-        </div>
-    </div>
-  );
-};
-
-export default AboutMe;
