0f75c7a0e569a9b996c803af11a87392ae1c2b25
Kishan Trivedi <trivedikishan12345@gmail.com>
2024-04-21 11:13:05 -0500

    Updated footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <p className={styles.footerText}>© 2024 - Designed by <a href="" className={styles.footerLink}>Kishan Trivedi</a></p>
+                        <p className={styles.footerText}>© 2024 - Designed and Developed by <a href="" className={styles.footerLink}>Kishan Trivedi</a></p>
                     </div>
                 </div>
             </div>
