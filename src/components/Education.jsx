8fca10709844ac0674a9e8830f9aea19417e3c72
Kishan Trivedi <trivedikishan12345@gmail.com>
2024-04-22 13:54:44 -0500

    Added animation to education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import ExperienceItem from './EducationItem'; // Make sure to import ExperienceItem
 import styles from './Education.module.css';
 
 const experiences = [
@@ -14,75 +15,16 @@ const experiences = [
 
 const Experience = () => {
     const [expandedId, setExpandedId] = useState(null);
-    const descriptionsRef = useRef({});
-
-    const handleExpand = (id) => {
-        setExpandedId(id);
-    };
-
-    const handleCollapse = () => {
-        setExpandedId(null);
-    };
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (expandedId !== null && descriptionsRef.current[expandedId]) {
-                if (!descriptionsRef.current[expandedId].contains(event.target)) {
-                    handleCollapse();
-                }
-            }
-        };
-
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [expandedId]); // Effect dependencies include expandedId
-
-    const renderDescription = (description, id) => {
-        const points = description.split('•').filter(point => point.trim());
-        const showMore = points.length > 3 && expandedId !== id; // Adjust the number of items to show when collapsed
-        const itemsToShow = showMore ? points.slice(0, 2) : points; // Show only the first 3 points if collapsed
-
-        return (
-            <ul ref={el => descriptionsRef.current[id] = el}>
-                {itemsToShow.map((point, index) => (
-                    <li key={index}>{point.trim()}</li>
-                ))}
-                {showMore && (
-                    <li>
-                        <a className={styles.readMoreButton} onClick={() => handleExpand(id)}>
-                            read more
-                        </a>
-                    </li>
-                )}
-            </ul>
-        );
-    };
 
     return (
-        <div id="education" className={`container justify-content-center mt-4 ${styles.experienceContainer}`}>
-            <h2 className={`text-center ${styles.experienceHeading} ${styles.sectionh2}`}>Education</h2>
+        <div id="experience" className={`container justify-content-center mt-4 ${styles.experienceContainer}`}>
+            <h2 className={`text-center ${styles.experienceHeading} ${styles.sectionh2}`}>Experience</h2>
             <div className={`row justify-content-center`}>
                 <div className="col-md-10 col-md-offset-1">
                     <div className={`${styles.experienceContent}`}>
                         <div className={`${styles.cdTimeline}`}>
-                            {experiences.map((experience) => (
-                                <div className={`${styles.cdTimelineBlock}`}
-                                    key={experience.id}>
-                                    <div className={`${styles.cdTimelineImg}`}>
-                                        <span className={`${styles.dot}`}></span>
-                                    </div>
-                                    <div className={`${styles.cdTimelineContent}`}>
-                                        <div className={`${styles.cdHeading}`}>
-                                            <h2>{experience.title}</h2>
-                                            <p>{experience.date} | <span>{experience.company}</span></p>
-                                        </div>
-                                        <div className={`${styles.cdDescription}`}>
-                                            {renderDescription(experience.description, experience.id)}
-                                        </div>
-                                    </div>
-                                </div>
+                            {experiences.map((experience, index) => (
+                                <ExperienceItem key={experience.id} experience={experience} index={index} expandedId={expandedId} setExpandedId={setExpandedId} />
                             ))}
                         </div>
                     </div>
---
+++ src/components/EducationItem.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef }  from 'react';
+import { useInView } from 'react-intersection-observer';
+import styles from './Education.module.css';
+
+const EducationItem = ({ experience, index, expandedId, setExpandedId }) => {
+    const [refHeading, inViewHeading] = useInView({
+        threshold: 0.4,
+        triggerOnce: true
+    });
+
+    const descriptionNode = useRef(null); 
+
+    const handleExpand = (id) => {
+        setExpandedId(id === expandedId ? null : id); // Toggle expanded state
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (descriptionNode.current && !descriptionNode.current.contains(event.target)) {
+                setExpandedId(null); // Collapse the description
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [setExpandedId, expandedId]);
+
+    const renderDescription = (description, id) => {
+        const points = description.split('•').filter(point => point.trim());
+        const showMore = points.length > 3 && expandedId !== id;
+        const itemsToShow = showMore ? points.slice(0, 2) : points;
+
+        return (
+            <ul>
+                {itemsToShow.map((point, index) => (
+                    <li key={index}>{point.trim()}</li>
+                ))}
+                {showMore && (
+                    <li>
+                        <a className={styles.readMoreButton} onClick={() => handleExpand(id)}>
+                            read more
+                        </a>
+                    </li>
+                )}
+            </ul>
+        );
+    };
+
+    const isEven = index % 2 === 0;
+    return (
+        <div className={`${styles.cdTimelineBlock}`}>
+            <div className={`${styles.cdTimelineImg}`}>
+                <span className={`${styles.dot}`}></span>
+            </div>
+            <div className={`${styles.cdTimelineContent}`}>
+                <div ref={refHeading} className={`${styles.cdHeading} ${inViewHeading ? (isEven ? styles.slideInFromLeftHeading : styles.slideInfromRightHeading) : ''}`}>
+                    <h2>{experience.title}</h2>
+                    <p>{experience.date} | <span>{experience.company}</span></p>
+                </div>
+                <div ref={refHeading} className={`${styles.cdDescription} ${inViewHeading ? (isEven ? styles.slideInFromRightDescription : styles.slideInFromLeftDescription) : ''}`}>
+                    {renderDescription(experience.description, experience.id)}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EducationItem;
